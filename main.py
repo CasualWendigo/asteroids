c81bf037f49290c087354242f02e4f39f4d4a52e
Hurho the Wendigo <ajbrooksdesign@gmail.com>
2025-08-08 21:17:52 -0400

    Base Mechanics Added

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
-from constants import SCREEN_WIDTH
-from constants import SCREEN_HEIGHT
+from player import Player
+from constants import *
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -8,16 +11,46 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    dt = 0
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    while 1 > 0:
+    shots = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (updatable, drawable, shots)
+
+    player = Player((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2))
+    asteroid_field = AsteroidField()
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        updatable.update(dt)
+        for asteroid in asteroids:
+            if player.has_collided(asteroid):
+                print("Game over!")
+                return
+            for shot in shots:
+                if shot.has_collided(asteroid):
+                    asteroid.split()
+                    shot.kill()
 
         screen.fill("black")
+
+        for sprite in drawable:
+            sprite.draw(screen)
+
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
